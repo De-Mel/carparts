9f1a537da6732ee2513d0a3ee3247884ad581aaf
De-Mel <30821269+De-Mel@users.noreply.github.com>
2022-08-16 16:06:39 +0600

    Add files via upload

--- php/send_mail.php
+++ php/send_mail.php
@@ -2,6 +2,30 @@
 $to = "as_saint@mail.ru";
 $from = trim($_POST['email']);
 
+$name = htmlspecialchars($_POST['name']);
+$name = urldecode($name);
+$name = trim($name);
+
+$email = htmlspecialchars($_POST['email']);
+$email = urldecode($email);
+$email = trim($email);
+
+$tel = htmlspecialchars($_POST['tel']);
+$tel = urldecode($tel);
+$tel = trim($tel);
+
+$brand = htmlspecialchars($_POST['brand']);
+$brand = urldecode($brand);
+$brand = trim($brand);
+
+$model = htmlspecialchars($_POST['model']);
+$model = urldecode($model);
+$model = trim($model);
+
+$vin = htmlspecialchars($_POST['vin']);
+$vin = urldecode($vin);
+$vin = trim($vin);
+
 $manners = htmlspecialchars($_POST['manners']);
 $manners = urldecode($manners);
 $manners = trim($manners);
